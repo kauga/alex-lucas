a4d23a51142f9b41cd9e43e20f2bec68e75edfab
kauga <alexlucas1944@gmail.com>
2024-06-09 12:48:39 +0300

    full application

---
+++ script.js
@@ -0,0 +1,50 @@
+"use strict";
+const heighty = document.querySelector('.logoLink');
+const targetArea = document.querySelector(".main-link");
+const menuOpenButton = document.querySelector(".menuOpenButton");
+const menucloseButton = document.querySelector(".closeNav");
+
+menuOpenButton.addEventListener('click', function() {
+    targetArea.classList.add('hidden');
+    console.log('something going wrong after clicked');
+})
+
+menucloseButton.addEventListener('click', function() {
+    console.log('marekebisho kidogo')
+
+    targetArea.classList.remove('hidden');
+});
+
+document.querySelector(".main-link").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("navLink")) {
+    const id = e.target.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// sticky navigation 
+const header = document.querySelector('.section--one');
+const targetheight = heighty.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) {
+    heighty.classList.add("sticky")
+    console.log('sticky')
+  } else {
+    heighty.classList.remove("sticky");
+    console.log('hhh')
+  }
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${targetheight}px`,
+});
+
+headerObserver.observe(header);
